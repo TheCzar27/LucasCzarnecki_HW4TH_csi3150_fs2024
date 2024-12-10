2b90ce93e14a7ec6522b8e949effbd7464b81b74
TheCzar27 <Lucasczar27@gmail.com>
2024-12-10 09:54:35 -0500

    Final Commit

---
+++ js/button_Press.js
@@ -0,0 +1,47 @@
+function onButtonPress() {
+  let randomVar = Math.floor(Math.random() * 10);
+  switch (randomVar) {
+    case 0:
+      alert(
+        "Denial is the ultimate comfort zone - David Goggins"
+      );
+      break;
+    case 1:
+      alert("You miss 100% of the shots you don't take - Wayne Gretzky");
+      break;
+    case 2:
+      alert(
+        "Hard work beats talent when talent doesn't work hard - Tim Notke"
+      );
+      break;
+    case 3:
+      alert("The only way to do great work is to love what you do -Steve Jobs");
+      break;
+    case 4:
+      alert("If you are going through hell, keep going - Winston Churchill");
+      break;
+    case 5:
+      alert("Mental toughness is a lifestyle - David Goggins");
+      break;
+    case 6:
+      alert(
+        "Life has no limitations, except the ones you make - Les Brown"
+      );
+      break;
+    case 7:
+      alert(
+        "The future belongs to those who believe in the beauty of their dreams - Eleanor Roosevelt"
+      );
+      break;
+    case 8:
+      alert(
+        "You've got to love what you're doing. If you love it, you can overcome any handicap, or the soreness, or all the aches and pains and continue to play for a long, long time - Gordie Howe"
+      );
+      break;
+    case 9:
+      alert(
+        "Try not to become a man of success, but rather try to become a man of value - Albert Einstein"
+      );
+      break;
+  }
+}
